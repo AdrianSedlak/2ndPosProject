960424406e496326bd2191306864b1b9989afa03
AdrianSedlak <sed19033@spengergasse.at>
2020-04-10 19:36:52 +0200

    Login Screen

---
+++ Sedlak_PosProjekt/Sedlak_PosProjekt/Login.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sedlak_PosProjekt
+{
+    /// <summary>
+    /// Interaktionslogik für Login.xaml
+    /// </summary>
+    public partial class Login : Window, IClosable
+    {
+        public Login()
+        {
+            InitializeComponent();
+            DataContext = new VMLogin();
+        }
+    }
+}
